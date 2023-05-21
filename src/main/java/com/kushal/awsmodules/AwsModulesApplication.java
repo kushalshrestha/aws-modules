3753cce5ea7e8df5b52dc0837b3a4c80b09c109b
kushalshrestha <kushalshr@gmail.com>
2023-05-20 23:35:29 -0500

    feat: Create, List buckets

--- src/main/java/com/kushal/awsmodules/AwsModulesApplication.java
+++ src/main/java/com/kushal/awsmodules/AwsModulesApplication.java
@@ -6,23 +6,15 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 import com.kushal.awsmodules.s3.service.AWSS3Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AwsModulesApplication implements CommandLineRunner {
-//    private static final AWSCredentials credentials;
-
-    static {
-    }
-
     @Value("${aws.s3.bucketName}")
     private String bucketName;
     @Value("${aws.accesskey}")
@@ -34,20 +26,30 @@ public class AwsModulesApplication implements CommandLineRunner {
         SpringApplication.run(AwsModulesApplication.class, args);
     }
 
-
     @Override
     public void run(String... args) throws Exception {
 
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         //AmazonS3 client setup
-        AmazonS3 s3client = AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.US_EAST_2)
-                .build();
+        AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                                                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                                                 .withRegion(Regions.US_EAST_2)
+                                                 .build();
+        AWSS3Service awsService = new AWSS3Service(s3client);
 
+        //creating a bucket
+        if (awsService.doesBucketExist(bucketName)) {
+            System.out.println("Bucket already exist. Skipping bucket creation");
+        } else {
+            awsService.createBucket(bucketName);
+        }
 
+        //list all the buckets
+        System.out.println("-------------Listing all bucket-------------");
+        for (Bucket s : awsService.listBuckets()) {
+            System.out.println(s.getName());
+        }
 
 
     }
