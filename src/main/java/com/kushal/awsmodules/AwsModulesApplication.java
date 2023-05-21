5175b675dec7a8f92ceead95a02dff16890c9ab5
kushalshrestha <kushalshr@gmail.com>
2023-05-20 21:25:04 -0500

    Project setup

---
+++ src/main/java/com/kushal/awsmodules/AwsModulesApplication.java
@@ -0,0 +1,13 @@
+package com.kushal.awsmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsModulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushal/awsmodules/AwsModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushal.awsmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
