659703e959e28a8ca022dcfe9f7a7b168dfdac18
kushalshrestha <kushalshr@gmail.com>
2023-05-31 21:18:40 -0500

    AWS SNS project initialization

---
+++ sns/src/test/java/com/kushal/sns/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushal.sns.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
