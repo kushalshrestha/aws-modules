742ed7a36161ab91dd836cb11b78fb4590e30162
kushalshrestha <kushalshr@gmail.com>
2023-06-03 15:43:08 -0500

    feat: Lambda function with input List and returning List

--- awslambda/HelloWorldFunction/src/main/java/com/kushal/aws/lambda/DataTypes.java
+++ awslambda/HelloWorldFunction/src/main/java/com/kushal/aws/lambda/DataTypes.java
@@ -1,13 +1,36 @@
 package com.kushal.aws.lambda;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class DataTypes {
 
     /**
      * Creating a lambda function method with input param float and return type of int
+     *
      * @param number
      * @return
      */
     public int getNumber(float number) {
         return (int) number;
+    }
+
+    public boolean isGreaterThan100(float number) {
+        return number > 100;
+    }
+
+    public List<Integer> getScores(List<String> names) {
+        Map<String, Integer> studentScores = new HashMap<>();
+        studentScores.put("John", 90);
+        studentScores.put("Bob", 80);
+        studentScores.put("Kushal", 100);
+
+        List<Integer> matchingScores = new LinkedList<>();
+        names.forEach(name -> {
+            matchingScores.add(studentScores.get(name));
+        });
+        return matchingScores;
     }
 }
